75b0561abc09e7b8c87b70daabad4742cb41882e
samat <samat@192.168.0.15>
2022-04-26 16:51:33 -0700

    Title: First commit of all the maven files

---
+++ mavenproject1/src/test/java/package1/class1.java
@@ -0,0 +1,20 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class class1 {
+
+	@Test
+	public void method1 () {
+		System.out.println("testing maven project: Method1.");
+	}
+	
+	@Test
+	public void method2 () {
+		System.out.println("testing maven project: Method2.");
+	}
+	@Test
+	public void method3 () {
+		System.out.println("testing maven project: Method3.");
+	}
+}
