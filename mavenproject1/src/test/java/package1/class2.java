980558d3f247a5e18591ecfbd126033dd0896c62
samat <samat@192.168.0.15>
2022-04-27 05:32:55 -0700

    commiting maven code

--- mavenproject1/src/test/java/package1/class2.java
+++ mavenproject1/src/test/java/package1/class2.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class class2 {
 	@Test
 	public void method12 () {
-		System.out.println("testing maven  project: Method12. ");
+		System.out.println("testing maven project: Method12. ");
 	}
 	
 	@Test
