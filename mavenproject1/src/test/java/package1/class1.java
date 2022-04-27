8f73e0b203ca020aa3676963caa69138b0ec68ad
samat <samat@192.168.0.15>
2022-04-26 17:05:47 -0700

    Title: First commit

--- mavenproject1/src/test/java/package1/class1.java
+++ mavenproject1/src/test/java/package1/class1.java
@@ -6,15 +6,15 @@ public class class1 {
 
 	@Test
 	public void method1 () {
-		System.out.println("testing maven project: Method11.");
+		System.out.println("testing maven project: Method11");
 	}
 	
 	@Test
 	public void method2 () {
-		System.out.println("testing maven project: Method21.");
+		System.out.println("testing maven project: Method21");
 	}
 	@Test
 	public void method3 () {
-		System.out.println("testing maven project: Method31.");
+		System.out.println("testing maven project: Method31");
 	}
 }
